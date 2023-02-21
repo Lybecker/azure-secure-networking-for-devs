cabe41b5aabd4802ff54591821eb2a466f5d8bf8
Anders Lybecker <anders@lybecker.com>
2023-02-21 13:11:15 +0100

    Using proper naming

--- demo1-create-vnet-subnets.sh
+++ demo1-create-vnet-subnets.sh
@@ -1,9 +1,9 @@
 # Expect a resource group named Networking
 
 # Create a VNET with 65K addresses
-az network vnet create --resource-group Networking --name MyVnet --address-prefix 10.1.0.0/16
+az network vnet create --resource-group rg-networking --name vnet-demo --address-prefix 10.1.0.0/16
 
 # Create a Subnet with 256(251) addresses from the VNET (can create 256 subnets of same size)
-az network vnet subnet create --resource-group Networking --name MySubnet0 --vnet-name MyVnet --address-prefixes 10.1.0.0/24
+az network vnet subnet create --resource-group rg-networking --name snet-0 --vnet-name vnet-demo --address-prefixes 10.1.0.0/24
 
-az network vnet subnet create --resource-group Networking --name MySubnet1 --vnet-name MyVnet --address-prefixes 10.1.1.0/24
+az network vnet subnet create --resource-group rg-networking --name snet-1 --vnet-name vnet-demo --address-prefixes 10.1.1.0/24
