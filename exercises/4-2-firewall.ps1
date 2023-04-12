45e71f44347ae1fb8e008578c609e43629bd3df2
Tomi Paananen <tomi.paananen@microsoft.com>
2023-04-12 14:45:33 +0300

    Minor change

--- exercises/4-2-firewall.ps1
+++ exercises/4-2-firewall.ps1
@@ -7,7 +7,7 @@ $Environment = "dev"
 $ResourceGroupName = "rg-${TeamName}-${Environment}"
 $FirewallName = "afw-${TeamName}-${Environment}-${Location}"
 $FirewallPublicIpAddressName = "pip-firewall-${TeamName}-${Environment}-${Location}"
-$FirewallConfigName = "config-${FirewallName}"
+$FirewallPublicIpConfigName = "config-${FirewallPublicIpAddressName}"
 $VnetName = "vnet-${TeamName}-${Environment}-${Location}"
 
 .\2-1-subnet.ps1 $TeamName $Location "firewall" "10.0.0.128/26"
@@ -35,7 +35,7 @@ Write-Output "`nConfiguring public IP for firewall..."
 
 az network firewall ip-config create `
     --firewall-name $FirewallName `
-    --name $FirewallConfigName `
+    --name $FirewallPublicIpConfigName `
     --public-ip-address $FirewallPublicIpAddressName `
     --resource-group $ResourceGroupName `
     --vnet-name $VnetName
@@ -51,6 +51,6 @@ az network public-ip show `
     --resource-group $ResourceGroupName
 
 # https://learn.microsoft.com/cli/azure/network/firewall/ip-config?view=azure-cli-latest#az-network-firewall-ip-config-list
-$FirewallPrivateIpAddress=(az network firewall ip-config list --resource-group $ResourceGroupName --firewall-name $FirewallName --query "[?name=='${FirewallConfigName}'].privateIpAddress" | ConvertFrom-Json)
+$FirewallPrivateIpAddress=(az network firewall ip-config list --resource-group $ResourceGroupName --firewall-name $FirewallName --query "[?name=='${FirewallPublicIpConfigName}'].privateIpAddress" | ConvertFrom-Json)
 
 Write-Output "`nFirewall private IP address: ${FirewallPrivateIpAddress}"
