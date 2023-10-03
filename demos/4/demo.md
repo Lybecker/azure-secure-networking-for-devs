423a08b55798e86735077bc61a889e6ad470185d
Anders Lybecker <anlybeck@microsoft.com>
2023-10-03 13:40:09 +0200

    Demo done with one subnet

--- demos/4/demo.md
+++ demos/4/demo.md
@@ -1,12 +1,20 @@
 # Routes & network security demo
 
-Run the [setup.ps1](setup.ps1) script, which creates 
+Run the [setup.ps1](setup.ps1) script, which creates
+:
 
-- two virtual peered networks (Corp website and Hub)
-- a virtual machine that acts as a web server in the Corp website virtual network. The virtual machine has public RDP access for demo purposes.
+- Two virtual peered networks (Corp website and Hub)
+- Virtual machine that acts as a web server in the Corp website virtual network.
 - Azure SQL with private link in the Corp website virtual network
+- Azure Firewall
+- Azure Bastion
 
 All demos are in the Azure portal.
+
+The web server VM `vm-web-dev` is used to demonstrate the scenarios. Login via Bastion with username `azureuser` and your local ssh private key found in the following locations:
+
+- `C:\Users\<username>\.ssh\id_rsa`
+- `~/.ssh/id_rsa`
 
 ## System routes
 
@@ -23,18 +31,54 @@ Azure automatically adds routes to the system route table when you create a virt
 
 ## Network segmentation with NSG and ASG
 
-1. Create ASG asg-web-role and assign it to the nic of the web server/VM
-1. Create ASG asg-db-role and assign in the the (SQL -> Networking -> Private access -> pep-sql-corpwebsite -> Application security groups)
-1. Create NSG nsg-corpwebsite-dev-swedencentral and associate with subnet snet-corpwebsite-dev-swedencentral. Because the AllowVNetInBound default security rule allows all communication between resources in the same virtual network, this rule is needed to deny traffic from all resources.
-    - Create rule Deny-Database-All
+1. Create ASG `asg-web-role` and assign it to the nic of the web server/VM
+1. Create ASG `asg-db-role` and assign in the the (SQL -> Networking -> Private access -> pep-sql-corpwebsite -> Application security groups)
+1. Create NSG `nsg-corpwebsite-dev-swedencentral` and associate with subnet `snet-corpwebsite-dev-swedencentral`. Because the AllowVNetInBound default security rule allows all communication between resources in the same virtual network, this rule is needed to deny traffic from all resources.
+    - Create rule `Deny-Database-All`
     - with priority 120
-    - deny all traffic to asg-db-role
-1. Show it is not possible to access the database from the web server via RDP
+    - deny all traffic to `asg-db-role`
+1. Show it is not possible to access the database from the web server
 
-    `Test-NetConnection -ComputerName 'sql-corpwebsite-dev-swedencentral.database.windows.net' -Port 1433`
-1. Create rule Allow-Database-BusinessLogic
+    ```bash
+    telnet sql-corpwebsite-dev-swedencentral.database.windows.net 1433
+    ```
+
+1. Create rule `Allow-Database-BusinessLogic`
     - with priority 110
-    - allow traffic from asg-web-role to asg-db-role
-1. Show it is now possible to access the database from the web server via RDP
+    - allow traffic from `asg-web-role` to `asg-db-role`
+1. Show it is now possible to access the database from the web server
 
-    `Test-NetConnection -ComputerName 'sql-corpwebsite-dev-swedencentral.database.windows.net' -Port 1433`
+    ```bash
+    telnet sql-corpwebsite-dev-swedencentral.database.windows.net 1433
+    ```
+
+## Firewall
+
+By default the Firewall allows no traffic. You need to create rules to allow traffic. We already created and outbound network rule allowing DNS lookups.
+
+- NAT rules - allows you to share network services with external networks. E.g. you can use a single public IP address to allow external clients to access multiple internal servers.
+- Network rules - non-HTTP/S traffic that will be allowed to flow through the firewall must have a network rule.
+- Application rules - HTTP/HTTPS traffic at Layer-7 network traffic filtering.
+
+> Rules are processed according to the rule type priority, but network rules are processed before application rules.
+
+1. Connect to the `vm-web-dev` and show you can do DNS lookups, but now HTTP requests
+
+    ```bash
+    nslookup www.google.com
+    nslookup www.microsoft.com
+    curl www.google.com
+    ```
+
+1. Show we have a network rule that allows DNS lookups
+1. Create new application rule collection `apprule-http` with priority 100
+    - with action Allow
+    - with rule type HTTP,HTTPS
+    - with source IP address *
+    - FQDN *.google.com
+1. Connect to the `vm-web-dev` and show HTTP requests to www.google.com is allowed
+
+    ```bash
+    curl www.google.com
+    curl www.microsoft.com
+    ```
--- demos/4/setup.ps1
+++ demos/4/setup.ps1
@@ -1,14 +1,15 @@
 $location = "swedencentral"
+$resourceGroup = "rg-corpwebsite-dev-${location}"
 
-Write-Output "Creating resource group rg-corpwebsite-dev-${location}"
+Write-Output "Creating resource group ${ResourceGroup}"
 az group create `
-    --name rg-corpwebsite-dev-${location}  `
+    --name $resourceGroup  `
     --location $location
 
 Write-Output "Creating virtual network vnet-corpwebsite-dev-${location}"
 az network vnet create `
     --name vnet-corpwebsite-dev-${location} `
-    --resource-group rg-corpwebsite-dev-${location} `
+    --resource-group $resourceGroup `
     --location $location `
     --address-prefix 10.0.0.0/16 `
     --subnet-name snet-corpwebsite-dev-${location} `
@@ -16,7 +17,7 @@ az network vnet create `
 
 az network vnet subnet update `
     --name snet-corpwebsite-dev-${location} `
-    --resource-group rg-corpwebsite-dev-${location} `
+    --resource-group $resourceGroup `
     --vnet-name vnet-corpwebsite-dev-${location} `
     --disable-private-endpoint-network-policies false `
     --disable-private-endpoint-network-policies false
@@ -24,7 +25,7 @@ az network vnet subnet update `
 Write-Output "Creating virtual network vnet-hub-dev-${location}"
 az network vnet create `
     --name vnet-hub-dev-${location} `
-    --resource-group rg-corpwebsite-dev-${location} `
+    --resource-group $resourceGroup `
     --location $location `
     --address-prefix 10.1.0.0/16 `
     --subnet-name snet-hub-dev-${location} `
@@ -33,32 +34,98 @@ az network vnet create `
 Write-Output "Peering vnet-corpwebsite-dev-${location} with vnet-hub-dev-${location}"
 az network vnet peering create `
     --name peer-corpwebsite-hub `
-    --resource-group rg-corpwebsite-dev-${location} `
+    --resource-group $resourceGroup `
     --vnet-name vnet-corpwebsite-dev-${location} `
     --remote-vnet vnet-hub-dev-${location} `
     --allow-vnet-access
 
 az network vnet peering create `
     --name peer-hub-corpwebsite `
-    --resource-group rg-corpwebsite-dev-${location} `
+    --resource-group $resourceGroup `
     --vnet-name vnet-hub-dev-${location} `
     --remote-vnet vnet-corpwebsite-dev-${location} `
     --allow-vnet-access
 
+Write-Output "Creating Bastion"
+az network vnet subnet create `
+    --name AzureBastionSubnet `
+    --resource-group $resourceGroup `
+    --vnet-name vnet-hub-dev-${location} `
+    --address-prefix 10.1.1.0/26 `
+    --disable-private-endpoint-network-policies false `
+    --disable-private-link-service-network-policies false
+
+az network public-ip create `
+    --resource-group $resourceGroup `
+    --name pip-bastion-dev-${location} `
+    --sku Standard `
+    --location $location
+
+az network bastion create `
+    --name bas-hub-dev-${location} `
+    --public-ip-address pip-bastion-dev-${location} `
+    --resource-group $resourceGroup `
+    --vnet-name vnet-hub-dev-${location} `
+    --location $location `
+    --sku Basic
+
+Write-Output "Creating Firewall"
+az network vnet subnet create `
+    --name AzureFirewallSubnet `
+    --resource-group $resourceGroup `
+    --vnet-name vnet-hub-dev-${location} `
+    --address-prefix 10.1.1.64/26 `
+    --disable-private-endpoint-network-policies true `
+    --disable-private-link-service-network-policies true
+
+az network firewall create `
+    --name fw-hub-dev-${location} `
+    --resource-group $resourceGroup `
+    --location $location
+
+az network public-ip create `
+    --resource-group $resourceGroup `
+    --name pip-fw-dev-${location} `
+    --sku Standard `
+    --location $location
+
+az network firewall ip-config create `
+    --firewall-name fw-hub-dev-${location} `
+    --name config-fw-hub-dev-${location} `
+    --public-ip-address pip-fw-dev-${location} `
+    --resource-group $resourceGroup `
+    --vnet-name vnet-hub-dev-${location}
+
+az network firewall update `
+    --name fw-hub-dev-${location} `
+    --resource-group $resourceGroup
+
+az network firewall network-rule create `
+    --collection-name net-base-col `
+    --destination-addresses 209.244.0.3 209.244.0.4 `
+    --destination-ports 53 `
+    --firewall-name fw-hub-dev-${location} `
+    --name Allow-DNS `
+    --protocols UDP `
+    --resource-group $resourceGroup `
+    --priority 200 `
+    --source-addresses * `
+    --action Allow
+
 Write-Output "Creating (managed SQL) database"
 az sql server create `
     --name sql-corpwebsite-dev-${location} `
-    --resource-group rg-corpwebsite-dev-${location} `
+    --resource-group $resourceGroup `
     --location $location `
     --admin-user sqladmin `
     --admin-password "LifeIsComplexSoShouldYourP@asswordBerb46w35b"
 
 Write-Output "Creating private endpoint for database"
-$id=$(az sql server list --resource-group rg-corpwebsite-dev-${location} --query '[].[id]' --output tsv)
+$id=$(az sql server list --resource-group $resourceGroup --query '[].[id]' --output tsv)
 
 az network private-endpoint create `
     --name pep-sql-corpwebsite `
-    --resource-group rg-corpwebsite-dev-${location} `
+    --resource-group $resourceGroup `
     --vnet-name vnet-corpwebsite-dev-${location} `
     --subnet snet-corpwebsite-dev-${location} `
     --private-connection-resource-id ${id} `
@@ -67,33 +134,33 @@ az network private-endpoint create `
 
 Write-Output "Configuring private DNS zone for database"
 az network private-dns zone create `
-    --resource-group rg-corpwebsite-dev-${location} `
+    --resource-group $resourceGroup `
     --name "privatelink.database.windows.net"
 
 az network private-dns link vnet create `
-    --resource-group rg-corpwebsite-dev-${location} `
+    --resource-group $resourceGroup `
     --zone-name "privatelink.database.windows.net" `
     --name dnsl-sql `
     --virtual-network vnet-corpwebsite-dev-${location} `
     --registration-enabled false
 
 az network private-endpoint dns-zone-group create `
-    --resource-group rg-corpwebsite-dev-${location} `
+    --resource-group $resourceGroup `
     --endpoint-name pep-sql-corpwebsite `
     --name zone-sql `
     --private-dns-zone "privatelink.database.windows.net" `
     --zone-name sql
 
-Write-Output "Creating VM with public IP to act as a web server"
+Write-Output "Creating VM to act as a web server"
 az vm create `
     --name vm-web-dev `
-    --resource-group rg-corpwebsite-dev-${location} `
+    --resource-group $resourceGroup `
     --image Ubuntu2204 `
     --size Standard_B2s `
     --vnet-name vnet-corpwebsite-dev-${location} `
     --subnet snet-corpwebsite-dev-${location} `
-    --public-ip-sku Standard `
     --admin-username azureuser `
+    --public-ip-address "" `
     --generate-ssh-keys
 
 Write-Output "Login to the VM via RDP and verify you can reach the database via Powershell command:"
@@ -102,6 +169,4 @@ Write-Output "Test-NetConnection -ComputerName 'sql-corpwebsite-dev-${location}.
 
 
 
-create subnet AzureFirewallSubnet
-Create FW with policy and public IP
 Route table - associate to subnet + add route to
