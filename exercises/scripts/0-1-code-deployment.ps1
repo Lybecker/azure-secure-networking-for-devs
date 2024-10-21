8fe74c421d622ea770079858acdd2afd2b8afeb6
Tomi Paananen <tomi.paananen@microsoft.com>
2024-10-21 06:09:29 +0300

    fix: code deployment script

--- exercises/scripts/0-1-code-deployment.ps1
+++ exercises/scripts/0-1-code-deployment.ps1
@@ -1,45 +1,25 @@
 #!/usr/bin/env pwsh
 
-param(
-    [string]$TeamName = $env:TEAM_NAME,
-    [string]$EuLocation = $env:EU_LOCATION,
-    [string]$UsLocation = $env:US_LOCATION,
-    [string]$HubLocation = $env:HUB_LOCATION
-)
+param([string]$TeamName = $env:TEAM_NAME)
 
 if ($TeamName.Length -lt 2) {
     Write-Error "Invalid argument: Team name missing or too short (must be at least 2 characters long)"
     exit 1
 }
 
-if ($EuLocation.Length -eq 0) {
-    Write-Error "Invalid argument: EU location missing"
-    exit 1
-}
+.\set-resource-names.ps1 -TeamName $TeamName -Environment "dev"
 
-if ($UsLocation.Length -eq 0) {
-    Write-Error "Invalid argument: US location missing"
-    exit 1
-}
-
-if ($HubLocation.Length -eq 0) {
-    Write-Error "Invalid argument: Hub location missing"
-    exit 1
-}
-
-Write-Output "`nUsing config:`n  - Team name: ${TeamName}`n  - EU location: ${EuLocation}`n  - US location: ${UsLocation}"
-
-$Environment = "dev"
-
-.\set-resource-names.ps1 -TeamName $TeamName -EuLocation $EuLocation -UsLocation $UsLocation -HubLocation $HubLocation -Environment $Environment
-
+$ResourceGroupNames = @($env:ASNFD_RESOURCE_GROUP_NAME_EU, $env:ASNFD_RESOURCE_GROUP_NAME_US)
 $AppServiceNames = @($env:ASNFD_APP_SERVICE_NAME_EU, $env:ASNFD_APP_SERVICE_NAME_US)
 
 for ($i = 0; $i -lt 2; $i++) {
-    Write-Output "Deploying code to app service `"$AppServiceNames[$i]`" in resource group `"$ResourceGroupNames[$i]`"..."
+    $ResourceGroupName = $ResourceGroupNames[$i]
+    $AppServiceName = $AppServiceNames[$i]
+
+    Write-Output "Deploying code to app service `"$AppServiceName`" in resource group `"$ResourceGroupName`"..."
 
     .\subscripts\0-1-deploy-code.ps1 `
-        -ResourceGroupName $ResourceGroupNames[$i] `
-        -AppServiceName $AppServiceNames[$i] `
+        -ResourceGroupName $ResourceGroupName `
+        -AppServiceName $AppServiceName `
         -CodePackagePath "../../src/web-app.zip"
 }
